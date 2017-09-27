f0afddf4b3e2b27034cecfe9c13751c6a5fab56d
Kaori Yatsu <s1210103@gmail.com>
2017-09-28 01:23:30 +0900

    [add]ex8's solution and .gitignore

---
+++ ex8.js
@@ -0,0 +1,14 @@
+var bl = require('bl')
+var http = require('http')
+
+http.get(process.argv[2],function(res){
+  // res.setEncoding("utf8");
+  res.pipe(
+    bl(
+      function (err, data) {
+        console.log(data.length);  
+        console.log(data.toString());
+      })
+    )
+})
+
