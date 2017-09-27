bffe2e1899b47991ceabde14fd4034b1a76e3282
Kaori Yatsu <s1210103@gmail.com>
2017-09-28 00:50:33 +0900

    [add]ex6's solution.

--- ex6.js
+++ ex6.js
@@ -1,4 +1,13 @@
 var hoge_module = require('./module')
 
-hoge_module();
+hoge_module(process.argv[2],process.argv[3], function(err, list){
+  if (err) {
+    console.log(err);
+  }
+  else {
+    list.forEach(function(file) {
+    console.log(file);
+  });
+  }
+});
 //console.log(hoge_module(process.argv[2],process.argv[3]));
--- module.js
+++ module.js
@@ -1,19 +1,18 @@
 var fs = require('fs')
 
-var data;
-function listFile(dir,fs,callback) {
-  fs.readdir(process.argv[2],function callback (err, list) {
+var data = []
+function listFile(dir, file, callback) {
+  fs.readdir(dir, function (err, list) {
     if (err) {
       return callback(err)
     }
     else{
       for (var i = 0; i < list.length; i++) {
-        if (list[i].split(".")[1] === process.argv[3]) { 
-          data.push(list[i]); 
-          console.log(data);
+        if (list[i].split(".")[1] === file) {
+           data.push(list[i]); 
         }
       }
-      callback(null, data)
+      callback(null,data);
     }
   })
 }
